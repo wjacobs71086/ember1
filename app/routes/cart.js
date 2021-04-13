1cff980587d0a62dc8b88c9cc70a94e00ab1a93d
wjacobs71086 <w.jacobs.632@gmail.com>
2021-04-12 20:52:26 -0700

    moved logic to the controller

--- app/routes/cart.js
+++ app/routes/cart.js
@@ -1,4 +1,17 @@
 import Route from '@ember/routing/route';
 
 export default class CartRoute extends Route {
+    model(){
+        const items = [{price: 100}, {price:150}];
+        return items;
+    }
+
+    setupController(controller, model) {
+        super.setupController(controller,model);
+        
+        // controller.set(
+        //     'subtotal', subtotal
+        // )
+    };
+
 }
